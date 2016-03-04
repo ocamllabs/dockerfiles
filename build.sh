f8d086aff22dd462ce607f4387928087b83efe11
Anil Madhavapeddy <anil@recoil.org>
2016-03-04 18:01:30 +0000

    update builder

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 dockerfile-gen -g \
   --remote ocl,https://github.com/ocamllabs/opam-repo-dev.git \
   --compilers 4.02.3+afl,4.02.1+modular-implicits,4.02.1+multicore,4.03.0+effects+native \
+  --distros debian-stable \
   utop mirage
